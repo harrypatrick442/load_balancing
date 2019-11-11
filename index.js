ea6cd565fc1037ec5a8536b4e1e013f016b1d8f6
harrypatrick442 <34253839+harrypatrick442@users.noreply.github.com>
2019-11-11 19:03:29 +0000

    implementing

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports={
+	ClientDataOrchestratorClient:require('./backend/ClientDataOrchestratorClient'),
+	ClientDataOrchestratorServer:require('./backend/ClientDataOrchestratorServer'),
+	ClientDataOrchestratorStrings:require('./backend/ClientDataOrchestratorStrings'),
+	IndexVersioning:require('./backend/IndexVersioning'),
+	PageAssetsOrchestrator:require('./backend/PageAssetsOrchestrator'),
+	PageAssetsOrchestratorStrings:require('./backend/PageAssetsOrchestratorStrings'),
+};
